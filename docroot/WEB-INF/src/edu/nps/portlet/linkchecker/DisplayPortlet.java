9e8d9c6111707fa50a80324dff8fc2569e3baa16
Craig Vershaw <cmversha@nps.edu>
2014-03-26 14:52:23 -0700

    Initial commit

---
+++ docroot/WEB-INF/src/edu/nps/portlet/linkchecker/DisplayPortlet.java
@@ -0,0 +1,11 @@
+package edu.nps.portlet.linkchecker;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class DisplayPortlet
+ */
+public class DisplayPortlet extends MVCPortlet {
+ 
+
+}
